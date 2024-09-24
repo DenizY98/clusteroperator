62b90298ba3cb994c67b5f2340476041e73e0bf9
Deniz Yueksel <deniz.yueksel@trumpf.com>
2024-09-24 15:31:34 +0200

    fix(crd): added shortname

--- api/v1/edgecluster_types.go
+++ api/v1/edgecluster_types.go
@@ -20,9 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // EdgeClusterSpec defines the desired state of EdgeCluster
 type EdgeClusterSpec struct {
 	// Unique identifier for the edge cluster
@@ -50,6 +47,7 @@ type EdgeClusterStatus struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:shortName=ec
 // +kubebuilder:subresource:status
 
 // EdgeCluster is the Schema for the edgeclusters API
